bfecf72bee38f4411312046b5434e28d999b9001
Tan Hoa <letanhoa2003@gmail.com>
2024-01-23 14:54:10 +0700

    feat: Create product order interface Implemented a user-friendly order interface with the following features: - User can input recipient information for shipping. - User can select and review the list of products in their order. - Added functionality to enter a coupon code for discounts. - Included a 'Order' button to finalize the purchase.

--- ShopApp-Angular/src/app/app.module.ts
+++ ShopApp-Angular/src/app/app.module.ts
@@ -3,7 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { HomeComponent } from './home/home.component';
 import { HeaderComponent } from './header/header.component';
 import { FooterComponent } from './footer/footer.component';
-// import { OrderComponent } from './order/order.component';
+import { OrderComponent } from './order/order.component';
 // import { DetailProductComponent } from './detail-product/detail-product.component';
 // import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
 // import { LoginComponent } from './login/login.component';
@@ -13,17 +13,18 @@ import { FooterComponent } from './footer/footer.component';
   declarations: [    
     HomeComponent,
     HeaderComponent,
-    FooterComponent
-    // OrderComponent, DetailProductComponent, OrderConfirmComponent, LoginComponent, RegisterComponent
+    FooterComponent,
+    OrderComponent
+    // DetailProductComponent, OrderConfirmComponent, LoginComponent, RegisterComponent
   ],
   imports: [
     BrowserModule
   ],
   providers: [],
   bootstrap: [
-    HomeComponent
+    //HomeComponent
     //DetailProductComponent,
-     //OrderComponent,
+     OrderComponent
     //OrderConfirmComponent,
     //LoginComponent,
     // RegisterComponent
