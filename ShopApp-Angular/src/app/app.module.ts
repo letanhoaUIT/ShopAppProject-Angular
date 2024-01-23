054ff584a1fbe4ba1e8b36deb7500a5764557030
Tan Hoa <letanhoa2003@gmail.com>
2024-01-23 14:04:10 +0700

    feat: Create Home page with product search bar, category listings, and product details
    Developed the Home page with the following features:
    - Added a search bar to facilitate product search.
    - Implemented category listings to organize products.
    - Displayed product details including name, selling price, and ratings.
    - Included 'Add to Cart' and 'Buy Now' buttons for easy purchasing.
    
    This commit enhances the user experience by providing a dynamic Home page with efficient product navigation and quick purchasing options.

---
+++ ShopApp-Angular/src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HomeComponent } from './home/home.component';
+// import { HeaderComponent } from './header/header.component';
+// import { FooterComponent } from './footer/footer.component';
+// import { OrderComponent } from './order/order.component';
+// import { DetailProductComponent } from './detail-product/detail-product.component';
+// import { OrderConfirmComponent } from './order-confirm/order-confirm.component';
+// import { LoginComponent } from './login/login.component';
+// import { RegisterComponent } from './register/register.component'
+
+@NgModule({
+  declarations: [    
+    HomeComponent
+    // HeaderComponent,
+    // FooterComponent, 
+    // OrderComponent, DetailProductComponent, OrderConfirmComponent, LoginComponent, RegisterComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  bootstrap: [
+    HomeComponent
+    //DetailProductComponent,
+     //OrderComponent,
+    //OrderConfirmComponent,
+    //LoginComponent,
+    // RegisterComponent
+  ]
+})
+export class AppModule { }
