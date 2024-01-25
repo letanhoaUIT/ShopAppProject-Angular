a554a1b574cf926b42c138c652ef153ab9671e27
Tan Hoa <letanhoa2003@gmail.com>
2024-01-25 23:38:06 +0700

    Create Data Transfer Object (DTO) for project

--- ShopApp-Backend/shopapp/shopapp/src/main/java/com/project/shopapp/controllers/CategoryController.java
+++ ShopApp-Backend/shopapp/shopapp/src/main/java/com/project/shopapp/controllers/CategoryController.java
@@ -1,19 +1,24 @@
-package com.project.shopapp;
+package com.project.shopapp.controllers;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import com.project.shopapp.dtos.CategoryDTO;
 
 @RestController
 @RequestMapping("api/v1/categories")
 public class CategoryController {
     //hien tat ca cac categories
-    @GetMapping("") //http://localhost:8089/api/v1/categories
-    public ResponseEntity<String> getAllCategories() {
-        return ResponseEntity.ok("chao ban");
+    @GetMapping("") //http://localhost:8089/api/v1/categories?=1&limit=10
+    public ResponseEntity<String> getAllCategories(
+            @RequestParam("page")   int page,
+            @RequestParam("limit")  int limit
+    ) {
+        return ResponseEntity.ok(String.format("getAllCategories, page = %d, limit = %d", page, limit));
     }
 
     @PostMapping("")
-    public ResponseEntity<String> insertCategory() {
+    //Neu tham số truyền vào là 1 object thì sao? => Data Transfer Object = Request Object
+    public ResponseEntity<String> insertCategory(@RequestBody CategoryDTO categoryDTO) {
         return ResponseEntity.ok("This is insertCategory");
     }
 
---
+++ ShopApp-Backend/shopapp/shopapp/src/main/java/com/project/shopapp/dtos/CategoryDTO.java
@@ -0,0 +1,12 @@
+package com.project.shopapp.dtos;
+
+import lombok.*;
+
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class CategoryDTO {
+    private String name;
+}
