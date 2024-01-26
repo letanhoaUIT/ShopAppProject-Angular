ec8f794789a4da8ebe28b3fec7fd94edcafe01d3
Tan Hoa <letanhoa2003@gmail.com>
2024-01-27 00:11:15 +0700

    Create actions with ProductController

---
+++ ShopApp-Backend/shopapp/shopapp/src/main/java/com/project/shopapp/controllers/ProductController.java
@@ -0,0 +1,55 @@
+package com.project.shopapp.controllers;
+
+import com.project.shopapp.dtos.*;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+import jakarta.validation.*;
+
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/products")
+public class ProductController {
+    @PostMapping("")
+    //POST http://localhost:8088/v1/api/products
+    public ResponseEntity<?> createProduct(
+            @Valid @RequestBody ProductDTO productDTO,
+            BindingResult result
+    ) {
+        try {
+            if(result.hasErrors()) {
+                List<String> errorMessages = result.getFieldErrors()
+                        .stream()
+                        .map(FieldError::getDefaultMessage)
+                        .toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            return ResponseEntity.ok("Product created successfully");
+        } catch (Exception e) {
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
+
+    @GetMapping("")
+    public ResponseEntity<String> getProducts(
+            @RequestParam("page")     int page,
+            @RequestParam("limit")    int limit
+    ) {
+        return ResponseEntity.ok("getProducts here");
+    }
+    //http://localhost:8088/api/v1/products/6
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getProductById(
+            @PathVariable("id") String productId
+    ) {
+        return ResponseEntity.ok("Product with ID: " + productId);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProduct(@PathVariable long id) {
+        return ResponseEntity.ok(String.format("Product with id = %d deleted successfully", id));
+    }
+}
