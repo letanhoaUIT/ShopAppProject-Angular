e16bb66164d274c8a8c1d9b9914e48db9d142e1e
Tan Hoa <letanhoa2003@gmail.com>
2024-02-02 21:59:19 +0700

    Create Order and OrderDetail entity in models

---
+++ ShopApp-Backend/shopapp/shopapp/src/main/java/com/project/shopapp/models/Order.java
@@ -0,0 +1,67 @@
+package com.project.shopapp.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Entity
+@Table(name = "orders")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Order {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @Column(name = "fullname", length = 100)
+    private String fullName;
+
+    @Column(name = "email", length = 100)
+    private String email;
+
+    @Column(name = "phone_number",nullable = false, length = 100)
+    private String phoneNumber;
+
+    @Column(name = "address", length = 100)
+    private String address;
+
+    @Column(name = "note", length = 100)
+    private String note;
+
+    @Column(name="order_date")
+    private LocalDateTime orderDate;
+
+    @Column(name = "status")
+    private String status;
+
+    @Column(name = "total_money")
+    private Integer totalMoney;
+
+    @Column(name = "shipping_method")
+    private String shippingMethod;
+
+    @Column(name = "shipping_address")
+    private String shippingAddress;
+
+    @Column(name = "shipping_date")
+    private Date shippingDate;
+
+    @Column(name = "tracking_number")
+    private String trackingNumber;
+
+    @Column(name = "payment_method")
+    private String paymentMethod;
+
+    @Column(name = "active")
+    private Boolean active;//thuộc về admin
+
+}
